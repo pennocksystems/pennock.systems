dce0aa6f66c76729ca336619f33713940578651e
Pennock Systems <pennocksystems@gmail.com>
2026-07-10 11:44:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function renderLoading() {
   answerPanel.innerHTML = `
     <div class="gemini-loading">
   <div class="spark-mark">
-    <link rel="icon" href="../images/Logo.png" type="image/png" />
+    <link rel="logo-icon" href="../images/Logo.png" type="image/png" />
   </div>
 
   <p>Analyzing the prompt and preparing an automation plan...</p>
@@ -103,7 +103,7 @@ answerPanel.innerHTML = `
   <div class="gemini-result">
     <div class="gemini-result-head">
       <div class="spark-mark">
-        <link rel="icon" href="../images/Logo.png" type="image/png" />
+        <link rel="logo-icon" href="../images/Logo.png" type="image/png" />
       </div>
       <div>
         <strong>${demo.title}</strong>
