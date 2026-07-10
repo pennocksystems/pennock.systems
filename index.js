aebbd5a04a56716ff1fd243fbe5811364a47c595
Pennock Systems <pennocksystems@gmail.com>
2026-07-10 12:41:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function renderLoading() {
   answerPanel.innerHTML = `
     <div class="gemini-loading">
   <div class="spark-mark">
-    <img src="../images/Logo.png" alt="Logo" class="logo-icon">
+    <img src="Logo.png" alt="Logo" class="logo-icon">
   </div>
 
   <p>Analyzing the prompt and preparing an automation plan...</p>
